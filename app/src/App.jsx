719d03c0b4038b7c606903c98a5848ac359fe954
Unknown <sebastian.kruemling@gmail.com>
2017-05-01 23:05:39 +0200

    s

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,6 +1,5 @@
-import '../../node_modules/spectre.css/spectre.less';
-import '../../node_modules/spectre.css/spectre-exp.less';
-import '../../node_modules/spectre.css/spectre-icons.less';
+import '../css/custom.less';
+
 import '../css/style.css';
 
 import React from 'react';
@@ -9,11 +8,10 @@ import ApodContainer from './Container/ApodContainer';
 import { Provider } from 'react-redux';
 import store from './Store';
 
-
 // Render to ID content in the DOM
 ReactDOM.render(
-    <Provider store={store}>
-        <ApodContainer />
-    </Provider>,
-    document.getElementById('content')
+	<Provider store={store}>
+		<ApodContainer />
+	</Provider>,
+	document.getElementById('content')
 );
--- app/src/Components/ImagePane.jsx
+++ app/src/Components/ImagePane.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default class ImagePane extends React.Component {
 	render() {
-		return <h1>IMAGE</h1>;
+		return (
+			<div>
+				<h1>IMAGE</h1>
+				<button className="btn">Test</button>
+				<span>Das ist Text</span>
+				<div class="loading"></div>
+			</div>
+		);
 	}
 }
